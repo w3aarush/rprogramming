1bef3c799e95a59e39b9148141f618a6c9123cde
w3aarush <w3aarush@gmail.com>
2025-12-23 14:25:59 +0530

    performed simple linear regression

--- simpleLinearRegression.R
+++ simpleLinearRegression.R
@@ -4,4 +4,12 @@ y <- c(62, 85, 56, 21, 47, 17, 76, 92, 62, 58)
 
 relationship_model <- lm(y ~ x)
 
-print(relationship_model)
+# print(realationship_model)
+# print(summary(relationship_model))
+
+# PREDICTION
+z <- data.frame(x = 160)
+predict_result <- predict(relationship_model,z)
+print('0000000000000000000000000000000000000000000000000000000000000000')
+print(z)
+print(predict_result)
