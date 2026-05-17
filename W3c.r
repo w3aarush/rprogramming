07ddd8268f3aa6e337c51ac811313972e04128f3
Your Name <nirajkumar09160@gmail.com>
2026-05-17 16:57:14 +0530

    added by niraj

---
+++ W3c.r
@@ -0,0 +1,97 @@
+# ------------------------  Batch 1  --------------------------
+# for (x in 1:10){
+#     print(x)
+# }
+
+# this is use to calculate
+
+# name <- "Niraj"
+# age <- 45
+
+# print(name)
+# print(age)
+
+# name
+
+# ------------------------- Batch 2 -----------------------------
+
+# text <- "aewsome"
+# paste("R is ", text)
+
+# text1 <- "R is "
+# text2 <- "aewsome."
+# paste(text1, text2)
+
+# num1 <- 5
+# num2 <- 10
+# num1 + num2
+
+# # multiple variable 
+
+# var1 <- var2 <- var3 <- "Orange"
+
+# var1
+# var2
+# var3
+
+# my_var <- 30
+# my_var <- "Hello"
+
+# my_var
+
+# ------------------------- Batch 3 Data types ---------------------------------
+
+# x <-  10.5              
+# class(x)                    # numeric
+
+# x <- 10000L
+# class(x)                    # integer
+
+# x <- 9i + 5
+# class(x)                    # complex
+
+# c <- "R is Exicting"
+# class(c)                    # character / string
+
+# x <- TRUE
+# class(x)                    # Logical / boolean
+
+
+#   ------------------------------- Batch 4 - Numebrs ----------------------------
+
+# x = 10.5        # numeric
+# y <- 40L        # integer
+# z <- 4i         # complex
+
+# class(x)
+# class(y)
+# class(z)
+
+#  -------- Type conversion ---------
+
+# x <- 10000L             # inetger
+# y <- 5                  # numeric
+
+# # Convert from integer to numeric :
+# a <- as.numeric(x)  
+
+# # Convert from numeric tro integer
+# b <- as.integer(y)
+
+# class(a)
+# class(b)
+
+
+# -------------------------------- Batch 4 - Math ----------------------------------
+
+10 + 5
+
+max ( 10,45,87,99)
+min ( 41,25,36,45)
+
+sqrt(36)
+
+ceiling(1.4)
+floor(1.4)
+
+
